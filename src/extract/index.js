9b27f3bccf67535612b595fbefb939d5e8047a04
Artur Marchi <marchiartur@gmail.com>
2022-03-26 20:52:28 -0300

    Update index.js

--- src/extract/index.js
+++ src/extract/index.js
@@ -4,7 +4,9 @@ export async function extractHref(params) {
   try {
     const link = params?.link;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
 
     await page.goto(link);
